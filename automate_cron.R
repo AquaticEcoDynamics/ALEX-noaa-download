e271cf8ecc5ab8ec3a37dacfcbfbc5a942529de9
rqthomas <rqthomas@vt.edu>
2021-10-21 16:20:18 -0400

    adding stack

--- automate_cron.R
+++ automate_cron.R
@@ -1,4 +1,4 @@
-remotes::install_github("rqthomas/cronR")
+#remotes::install_github("rqthomas/cronR")
 #remotes::install_deps()
 library(cronR)
 
@@ -9,4 +9,12 @@ cmd <- cronR::cron_rscript(rscript = file.path(home_dir, "launch_download_downsc
                            log_append = FALSE,
                            workdir = file.path(home_dir))
 #trailing_arg = "curl -fsS -m 10 --retry 5 -o /dev/null https://hc-ping.com/cb249e47-f56b-45da-af7f-9c0c47db1a6c")
-cronR::cron_add(command = cmd, frequency = '0 */2 * * *', id = 'noaa_download')
+cronR::cron_add(command = cmd, frequency = '0 */4 * * *', id = 'noaa_download')
+
+cmd <- cronR::cron_rscript(rscript = file.path(home_dir, "run_stack_noaa.R"),
+                           rscript_log = file.path(home_dir, "noaa_stack.log"),
+                           log_append = FALSE,
+                           workdir = file.path(home_dir))
+#trailing_arg = "curl -fsS -m 10 --retry 5 -o /dev/null https://hc-ping.com/cb249e47-f56b-45da-af7f-9c0c47db1a6c")
+cronR::cron_add(command = cmd, frequency = 'daily', at = "6 am", id = 'noaa_stack')
+
