01ac84e51692352504ca302b7417d4be08ab308b
rqthomas <rqthomas@vt.edu>
2021-10-25 11:04:25 +0000

    finishing up s3 stack noaa

--- run_stack_noaa.R
+++ run_stack_noaa.R
@@ -5,26 +5,30 @@ dates <- seq(lubridate::as_date("2021-10-19"), Sys.Date() - lubridate::days(1), 
 test_dir <- "/home/rstudio/"
 
 #noaa_directory <- normalizePath(file.path(Sys.getenv("MINIO_HOME"), "drivers/noaa"))
-noaa_directory <- normalizePath(file.path(test_dir, "drivers/noaa"))
+noaa_directory <- normalizePath(file.path(test_dir, "drivers"))
 
 #Read list of latitude and longitudes
 neon_sites <- readr::read_csv("noaa_download_site_list.csv")
 
-noaa_model <- "NOAAGEFS_6hr"
+noaa_model <- "noaa/NOAAGEFS_6hr"
 output_directory <- noaa_directory
 model_name <- "observed-met-noaagefs"
 dates_w_errors <- c("2020-12-05","2020-12-06","2020-12-25")
+
+
 
 for(i in 1:length(neon_sites$site_id)){
   
   print(neon_sites$site_id[i])
 
-  noaaGEFSpoint::stack_noaa_forecasts(forecast_dates = dates,
+  stack_noaa_forecasts(forecast_dates = dates,
                        site = neon_sites$site_id[i],
                        noaa_directory = noaa_directory,
                        noaa_model = noaa_model,
                        output_directory = output_directory,
                        model_name = model_name,
-                       dates_w_errors = dates_w_errors)
+                       dates_w_errors = dates_w_errors,
+                       s3_mode = TRUE,
+                       bucket = "drivers")
 }
 
